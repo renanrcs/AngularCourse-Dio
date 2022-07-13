45d924b5450cb85be4d20e2fd07823cccb8ebe59
renanrcs <renan.cavalcante@yahoo.com>
2022-07-13 15:21:13 -0300

    trabalhando com pipes

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -1,10 +1,10 @@
 import { Component, OnInit } from "@angular/core";
-import { Course } from "./course";
+import { Course } from  './course';
 import { CourseService } from './course.service';
 
 @Component({
     selector: 'app-course-list',
-    templateUrl: './course-list.component.html',
+    templateUrl: './course-list.component.html'
 })
 export class CourseListComponent implements OnInit{
     
