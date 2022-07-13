c489ab98a2243d88bdd28d3b4c360e294e2fa37c
renanrcs <renan.cavalcante@yahoo.com>
2022-07-13 14:44:08 -0300

    tratando eventos data binding

--- src/app/courses/course-list.component.ts
+++ src/app/courses/course-list.component.ts
@@ -7,15 +7,29 @@ import { CourseService } from './course.service';
     templateUrl: './course-list.component.html',
 })
 export class CourseListComponent implements OnInit{
+    
+    filteredCourses : Course[] = [];
+    _courses : Course[] = [];
+    
+    _filterBy: string = '';
 
-    courses : Course[] = [];
 
     constructor (private courseService: CourseService) {
         
     }
 
     ngOnInit(): void{
-        this.courses = this.courseService.retrieveAll();
+        this._courses = this.courseService.retrieveAll();
+        this.filteredCourses = this._courses;
     }
 
+    set filter(value: string){
+        this._filterBy = value;
+
+        this.filteredCourses = this._courses.filter((course: Course) => course.name.toLocaleLowerCase().indexOf(this._filterBy.toLocaleLowerCase()) > -1);
+    }
+
+    get filterBy(){
+        return this._filterBy;
+    }
 }
